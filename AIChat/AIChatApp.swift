72d153bdb3b2b643ed35329ae45071abc901c4fe
IOSOM01 <om@unolo.com>
2026-02-20 23:55:30 +0530

    Initial Commit

---
+++ AIChat/AIChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIChatApp.swift
+//  AIChat
+//
+//  Created by OM BHAGWAN on 20/02/26.
+//
+
+import SwiftUI
+
+@main
+struct AIChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AIChatTests/AIChatTests.swift
@@ -0,0 +1,16 @@
+//
+//  AIChatTests.swift
+//  AIChatTests
+//
+//  Created by OM BHAGWAN on 20/02/26.
+//
+
+import Testing
+
+struct AIChatTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
